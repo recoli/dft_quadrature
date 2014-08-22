7b444b2c1ae1921055c2b23af15843259e2612e5
Sam Manzer <samuelmanzer@users.noreply.github.com>
2014-08-22 10:24:04 -0700

    Added first test stub

---
+++ tests/h2_test.py
@@ -0,0 +1,22 @@
+import unittest
+import math
+
+class TestIntegrateRhoH2(unittest.TestCase):
+
+    def slater_1s(zeta,R):
+        def slater_functor(r):
+            norm = math.sqrt(sum(pow(r[i] - R[i],2) for i in range(0,len(r))))
+            return (pow(zeta,3)/3)*math.exp(-zeta*norm)
+
+    def density(r):
+        return 2*pow(slater_functor(1,0) + slater_functor(1,.74),2)
+
+
+    def test_integrate_rho_h2(self):
+        atoms = [(0,0,0),(0,0,.74)]
+        quad = MolecularQuadrature(atoms)
+        n = quad.integrate(density)
+        self.assertEqual(n,2)
+
+if __name__ == '__main__':
+    unittest.main()
